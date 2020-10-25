772638b065998e51b289c846322970afdf38a322
David <sanchez.dav90@gmail.com>
2020-10-24 22:18:21 -0500

    Create User and Book types

---
+++ app/graphql/types/book_type.rb
@@ -0,0 +1,12 @@
+module Types
+  class BookType < Types::BaseObject
+    field :id, ID, null: false
+    field :user_id, Integer, null: false
+    field :title, String, null: true
+    field :author, String, null: true
+    field :publisher, String, null: true
+    field :genre, String, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
