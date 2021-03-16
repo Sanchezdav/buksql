69eb540f3dcc91d4887d0e2899f24af07ad0cdbb
David Sanchez <david.sanchez@kueski.com>
2021-03-16 14:20:11 -0600

    fix user into Book type

--- app/graphql/types/book_type.rb
+++ app/graphql/types/book_type.rb
@@ -5,7 +5,7 @@ module Types
     field :author, String, null: true
     field :publisher, String, null: true
     field :genre, String, null: true
-    field :user, [Types::UserType], null: true
+    field :user, Types::UserType, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
